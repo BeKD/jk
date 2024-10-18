3cf1d6d1fd590e39b83dbff649601a003b1fc2f8
czj <15603381912@163.com>
2024-10-18 17:34:03 +0800

    [fix]build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,14 @@ LOG_FILE="/data/logs/jk.log"
 # 指定端口
 PORT="18080"
 
+echo "构建开始..."
 mvn clean package
+echo "构建结束!"
 
+echo "复制开始..."
 cp ./target/*.jar $APP
+echo "复制结束!"
 
-nohup java -jar --server.port=$PORT $APP > $LOG_FILE 2>&1
+echo "程序启动..."
+nohup java -jar $APP --server.port=$PORT > $LOG_FILE 2>&1
+echo "程序启动成功!"
