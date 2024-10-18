b2ee5331c4d738efc6bffd420c99676f672f9336
czj <15603381912@163.com>
2024-10-18 17:16:16 +0800

    [fix]build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 # 部署目录
 APP_DIR="/data/myapp"
 
+# 应用名字
+APP_NAME="jk"
+
 # 日志目录
 LOG_DIR="/data/logs"
 
 mvn clean package
 
-cp ./target/*.jar $APP_DIR
+cp ./target/*.jar $APP_DIR/jk.jar
+
+# nohup java -jar  $APP_DIR/jk.jar >$LOG_NAME 2>&1 &
